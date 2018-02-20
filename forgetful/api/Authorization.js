59f676e50243d2b5ce1562d0cfb3aed935180c31
Evan Darst <evan.l.darst@gmail.com>
2018-02-20 08:37:58 -0500

    updating new file structure with pseudo for routes

---
+++ forgetful/api/Authorization.js
@@ -0,0 +1,15 @@
+//Const for requiring error handling
+
+//Check authorization against DB
+const tokens = {
+  test: //
+};
+//Add username
+}
+//Validate username
+
+//If authorized allow access
+
+//If not, throw error
+
+//Export
---
+++ forgetful/api/Routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//Router must require authorization validation
+
+//Router post for login
+
+//If username matches and password matches, allow access
+
+//Router use the User file
+
+//Throw error if unknown
+
+module.exports = router;
