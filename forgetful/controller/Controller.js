ce52d33be47f0631026e1ca9ccd2a5c4d5a8cf74
Evan Darst <evan.l.darst@gmail.com>
2018-02-19 20:26:04 -0500

    pseudo code for exports

--- forgetful/controller/Controller.js
+++ forgetful/controller/Controller.js
@@ -1,1 +1,8 @@
 const mongoose = require("mongoose");
+
+//Add an item to an existing list
+
+//Edit an item on an existing list
+
+//Delete an item on an existing list
+
---
+++ forgetful/model/Model.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+//require Schema
+
+//Holds the const for adding a new Schema
+
+//Export list
