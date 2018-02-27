d306d86d090da5407925d7dc5270ad36d4f8992a
Evan Darst <evan.l.darst@gmail.com>
2018-02-27 11:19:38 -0500

    save one request using router post

--- routes/tasks.js
+++ routes/tasks.js
@@ -20,19 +20,48 @@ router.get('/tasks', function(req, res, next) {
 
 });
 
-//Get one tasks
+//Get one task
 
 router.get('/task/:id', function(req, res, next){
 
     db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
 
         if(err){
-            
+
             res.send(err);
         }
 
         res.json(task);
     });
+});
+
+//Save tasks using post request
+
+router.post('/task', function(req, res, next){
+
+    const task = req.body;
+
+    if(!task.title || (task.isDone + '')){
+
+        res.status(400);
+        res.json({
+
+            "Error": "Bad request"
+        });
+    } else {
+
+        db.tasks.save(task, function(err, task){
+            
+            if(err){
+
+                res.send(err);
+            }
+    
+            res.json(task);
+
+        });
+    }
+
 });
 
 module.exports = router;
