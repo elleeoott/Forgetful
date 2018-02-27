b23ef6176c232bf65762ab2c7da98001cdcc463f
Evan Darst <evan.l.darst@gmail.com>
2018-02-27 11:15:18 -0500

    tasks.js now pulls all tasks or one task via API; hooked up via mongolab

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const mongojs = require('mongojs');
+const db = mongojs('mongodb://admin:admin@ds127341.mlab.com:27341/forgetful_db', ['tasks']);
+
+
+//Get all tasks
 
 router.get('/tasks', function(req, res, next) {
 
-    res.send('Task API');
+    db.tasks.find(function(err, tasks){
 
+        if(err){
+
+            res.send(err);
+        }
+
+        res.json(tasks);
+    });
+
+});
+
+//Get one tasks
+
+router.get('/task/:id', function(req, res, next){
+
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+
+        if(err){
+            
+            res.send(err);
+        }
+
+        res.json(task);
+    });
 });
 
 module.exports = router;
